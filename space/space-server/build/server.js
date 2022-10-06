b0ef94fc68c8d0998f8e0125bdf822b19ea5befe
Karandeep Virk <karandeep.virk@yahoo.com>
2022-10-05 19:16:40 -0700

    Added readFileSync Function

--- space/space-server/build/server.js
+++ space/space-server/build/server.js
@@ -6,10 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3001;
+const fs_1 = require("fs");
+const apache_arrow_1 = require("apache-arrow");
+const arrow = (0, fs_1.readFileSync)('simple.arrow');
+const table = (0, apache_arrow_1.tableFromIPC)(arrow);
 app.get("/data", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({ foo: "bar" });
 });
 app.listen(port, () => {
+    console.log("HELLO");
     console.log(`App is listenig to ${port}`);
 });
--- space/space-server/server.ts
+++ space/space-server/server.ts
@@ -1,6 +1,11 @@
 import express from 'express'
 const app = express()
 const port = 3001;
+import { readFileSync } from 'fs';
+import { tableFromIPC } from 'apache-arrow';
+
+const arrow = readFileSync('simple.arrow');
+const table = tableFromIPC(arrow);
 
 app.get("/data", (req,res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -8,5 +13,6 @@ app.get("/data", (req,res) => {
 });
 
 app.listen(port, () => {
+    console.log(table.toString());
     console.log(`App is listenig to ${port}`)
 })
